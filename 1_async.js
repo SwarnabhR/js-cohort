6d4889e284f9cf65bb56851559334a483d382a04
Swarnabh R. <workspace.swarnabh@gmail.com>
2025-06-15 21:58:18 +0530

    Async JS working

--- 1_async.js
+++ 1_async.js
@@ -17,14 +17,28 @@
 
 // console.log(sum(5));
 
-const fs = require("fs");
+// const fs = require("fs");
 
-const content = fs.readFile("beyza.txt", "utf-8", (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(data);
-});
+// const content = fs.readFile("beyza.txt", "utf-8", (err, data) => {
+//     if (err) {
+//         console.error(err);
+//         return;
+//     }
+//     console.log(data);
+// });
 
-console.log("Beyza lover");
+// console.log("Beyza lover");
+
+console.log("Hi");
+
+function timeout() {
+    console.log("Something");
+}
+
+setTimeout(timeout, 100);
+let c = 0;
+for (let i = 0; i < 1000000000; i++) {
+    c += i;
+}
+
+console.log("something");
